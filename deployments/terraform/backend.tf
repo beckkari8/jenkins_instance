8379eac0ec41b5dc9585d5ca7e12e3ad025ccf2c
beckkari8 <karimovbektur@gmail.com>
2020-08-02 06:26:53 -0500

    added correct folder

--- deployments/terraform/backend.tf
+++ deployments/terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
     backend "s3" {
         bucket = "jenkins-instance-beck"
-        key = "jenkins/us-east-1/class/dev/infrastructure.tfstate"
+        key = "jenkins/us-east-1/hw2/dev/infrastructure.tfstate"
         region = "us-east-1"
+ 
     }
 }
--- deployments/terraform/dev.tfvars
+++ deployments/terraform/dev.tfvars
@@ -2,6 +2,6 @@
 s3_bucket = "jenkins-instance-beck"
 s3_folder_project = "jenkins"
 s3_folder_region = "us-east-1"
-s3_folder_type = "class"
+s3_folder_type = "hw2"
 s3_tfstate_file = "infrastructure.tfstate"
 TF_LOG = ${var.DEBUG}
